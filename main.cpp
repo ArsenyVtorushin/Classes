d8684d3fe59d4b29191e3b8e1ae23ab04df28b03
Arseny Vtorushi <vtorushinarseny@yandex.ru>
2024-06-04 17:43:58 +0500

    1

--- main.cpp
+++ main.cpp
@@ -29,10 +29,25 @@ public:
 		Award();
 	}
 
+	void Print()
+	{
+		FillWorker();
+
+		std::cout << "\n******************************************************";
+
+		std::cout << std::left << std::setw(45) << "\n* Имя и фамилия: " << std::right << name 
+		 << std::left << std::setw(45) << "\n* Стаж (в годах): "            << std::right << experience 
+		 << std::left << std::setw(45) << "\n* Часовая зарплата: "          << std::right << wagesPerHour 
+		 << std::left << std::setw(45) << "\n* Кол-во отработанных часов: " << std::right << hoursInAll 
+		 << std::left << std::setw(45) << "\n* Итоговая зарплата: "         << std::right << salary 
+		 <<	std::left << std::setw(45) << "\n* Премия: "                    << std::right << award;
+
+		std::cout << "\n******************************************************\n";
+	}
+
 	void WagesInAll()
 	{
 		salary = wagesPerHour * hoursInAll;
-		std::cout << "\nЗарплата: " << salary;
 	}
 
 	void Award()
@@ -41,16 +56,14 @@ public:
 		{
 			award = salary / 100 * 5;
 		}
-		else if (experience > 3 && experience < 5)
+		else if (experience >= 3 && experience < 5)
 		{
 			award = salary / 100 * 8;
 		}
-		else if (experience > 5)
+		else if (experience >= 5)
 		{
 			award = salary / 100 * 15;
 		}
-
-		std::cout << "\nПремия: " << award << "\n\n";
 	}
 };
 
@@ -60,7 +73,7 @@ int main()
 	SetConsoleOutputCP(1251);
 
 	Worker First;
-	First.FillWorker();
+	First.Print();
 
 	return 0;
 }
